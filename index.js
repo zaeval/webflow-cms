fe317dc0c2c8f64479648ce10dd3d723bc26a0d7
zaeval <zaeval@108.software>
2023-10-28 14:59:28 +0900

    items offset error fixed

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import WebflowCMS from "./webflow-cms/bin";
-const token = "612ce0d9fb3db1aa4f76de760bb3df0d270a17164873c97d0cabac8810c39bb7";
+import WebflowCMS from "./webflow-cms/src";
 
 const setup = async () => {
     const webflowCMS = new WebflowCMS(token)
     const sites = await webflowCMS.getSites();
-    console.log(sites);
+    // console.log(sites);
     let collections = await webflowCMS.getCollections(sites[0]);
-    console.log(collections)
-    const collection = await webflowCMS.getCollectionByName(collections,"TestCollections");
+    // console.log(collections)
+    const collection = await webflowCMS.getCollectionByName(collections, "ImageBlock");
     console.log(collection._id);
     const items = await webflowCMS.getItems(collection)
     console.log(items);
-    const item = await webflowCMS.createItem(collection._id,{name:"testitem2",slug:"testitem2",testnumber:1234,_draft: false,_archived: false,})
+    console.log(await webflowCMS.getItemByName(collection,"f1385854-15a0-4c5d-82c2-48e6df0c079a"))
+    // const item = await webflowCMS.createItem(collection._id,{name:"testitem2",slug:"testitem2",testnumber:1234,_draft: false,_archived: false,})
 }
 setup();
--- webflow-cms/src/index.js
+++ webflow-cms/src/index.js
@@ -1,39 +1,61 @@
 const Webflow = require("webflow-api");
 const fetch = require('node-fetch');
 
-export default class WebflowCMS{
+export default class WebflowCMS {
     constructor(token) {
         this.webflow = new Webflow({
-            token:token
+            token: token
         })
         this.token = token
     }
-    async getSites(){
+
+    async getSites() {
         return (await this.webflow.info()).sites;
     }
-    async  getCollections (siteId){
+
+    async getCollections(siteId) {
         return await this.webflow.collections({siteId: siteId});
     }
-    async getCollectionByName (collections, name){
-        return collections.find((collection)=>{return collection.name == name})
+
+    async getCollectionByName(collections, name) {
+        return collections.find((collection) => {
+            return collection.name == name
+        })
     }
-    async getItems(collection){
-        return (await collection.items()).items;
+
+    async getItems(collection) {
+        let offset = 0;
+        let count = 100;
+        let ret = []
+        while (count == 100) {
+            const temp = await collection.items({offset:offset})
+            offset += temp.count
+            count = temp.count
+            ret = ret.concat(temp.items)
+        }
+        return ret
     }
-    async getItemByName (collection,name){
-        return (await collection.items()).items.find((item)=>{return item.name == name})
+
+    async getItemByName(collection, name) {
+        return (await this.getItems(collection)).find((item) => {
+            return item.name == name
+        })
     }
-    async updateItem(itemId,collectionId, data){
+
+    async updateItem(itemId, collectionId, data) {
         return await this.webflow.updateItem({
             itemId: itemId,
             collectionId: collectionId,
             fields: data
-        },{live:true});
+        }, {live: true});
     }
-    async createItem(collectionId,data){
+
+    async createItem(collectionId, data) {
         return await this.webflow.createItem({
-            collectionId:collectionId,
-            fields:data
-        },{live:true})
+            collectionId: collectionId,
+            fields: data
+        }, {live: true})
     }
 }
+
+
